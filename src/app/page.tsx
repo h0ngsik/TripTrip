0c2629d46c04d189515d99bc0f5738c76fed860e
h0ngsik <hongsik.choi.dev@gmail.com>
2025-02-12 21:20:31 +0900

    feat: 최초 접속 시 /boards 페이지로 이동하도록 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return (
-    <div>최상위 페이지입니다.</div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/boards");
+  }, []);
+
+  return <></>;
 }

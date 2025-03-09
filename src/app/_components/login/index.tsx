c33a8c5dabf665961cc17b0788dc7bea5acc347b
h0ngsik <hongsik.choi.dev@gmail.com>
2025-03-10 01:15:34 +0900

    #1-fix '[DOM ]Password field is not contained in a form' 오류 해결 - input type 중 passowrd 가 form 태그 없이 사용되어 경고 발 생

--- src/app/_components/login/index.tsx
+++ src/app/_components/login/index.tsx
@@ -24,48 +24,50 @@ export default function Login({ handleSignUp }: ILoginProps) {
 
   return (
     <FormProvider {...methods}>
-      <div className={styles.login__container}>
-        <div className={styles.logo__container}>
-          <LogoIcon width={7.5} height={5} />
-          <span className={styles.description}>
-            트립트립에 오신걸 환영합니다.
-          </span>
-        </div>
-        <div className={styles.input__container}>
-          <span className={styles.login__description}>
-            트립트립에 로그인 하세요.
-          </span>
-          <div className={styles.inputs}>
-            <InputForm<ILoginSchema>
-              validationStatus={isLoginFailed ? "error" : "default"}
-              keyName="email"
-              type="text"
-              placeholder="이메일을 입력해주세요."
+      <form onSubmit={methods.handleSubmit(onClickLogin)}>
+        <div className={styles.login__container}>
+          <div className={styles.logo__container}>
+            <LogoIcon width={7.5} height={5} />
+            <span className={styles.description}>
+              트립트립에 오신걸 환영합니다.
+            </span>
+          </div>
+          <div className={styles.input__container}>
+            <span className={styles.login__description}>
+              트립트립에 로그인 하세요.
+            </span>
+            <div className={styles.inputs}>
+              <InputForm<ILoginSchema>
+                validationStatus={isLoginFailed ? "error" : "default"}
+                keyName="email"
+                type="text"
+                placeholder="이메일을 입력해주세요."
+              />
+              <InputForm<ILoginSchema>
+                validationStatus={isLoginFailed ? "error" : "default"}
+                keyName="password"
+                type="password"
+                placeholder="비밀번호를 입력해주세요."
+                errorMessage={
+                  isLoginFailed ? "아이디 또는 비밀번호를 확인해 주세요." : ""
+                }
+              />
+            </div>
+          </div>
+          <div className={styles.buttons__container}>
+            <Button
+              label="로그인"
+              type="submit"
+              size={ButtonSize.large}
+              variant={ButtonVariant.primary}
+              style={{ width: "100%" }}
             />
-            <InputForm<ILoginSchema>
-              validationStatus={isLoginFailed ? "error" : "default"}
-              keyName="password"
-              type="password"
-              placeholder="비밀번호를 입력해주세요."
-              errorMessage={
-                isLoginFailed ? "아이디 또는 비밀번호를 확인해 주세요." : ""
-              }
-            />
+            <button className={styles.button__signUp} onClick={handleSignUp}>
+              회원가입
+            </button>
           </div>
         </div>
-        <div className={styles.buttons__container}>
-          <Button
-            label="로그인"
-            onClick={methods.handleSubmit(onClickLogin)}
-            size={ButtonSize.large}
-            variant={ButtonVariant.primary}
-            style={{ width: "100%" }}
-          />
-          <button className={styles.button__signUp} onClick={handleSignUp}>
-            회원가입
-          </button>
-        </div>
-      </div>
+      </form>
     </FormProvider>
   );
 }

fd33057ed470c74ae9d53f985a060c41d5948822
h0ngsik <hongsik.choi.dev@gmail.com>
2025-03-28 17:43:35 +0900

    #13-feat: '오늘 핫한 트립토크' 컴포넌트 UI 및 기본 기능 구현

--- src/app/boards/page.tsx
+++ src/app/boards/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import styles from "./styles.module.css";
+import { useBoards } from "./hook";
+
+import HotBoards from "../_components/boards/Boards-list/list/components/HotBoards";
+import SearchBar from "../_components/boards/Boards-list/list/components/Search";
 import BoardsList from "../_components/boards/Boards-list/list";
 import Pagination from "../_components/boards/Boards-list/list/components/Pagination";
-import { useBoards } from "./hook";
-import SearchBar from "../_components/boards/Boards-list/list/components/Search";
+import styles from "./styles.module.css";
 
 export default function BoardsPage() {
   const { boards, boardsCount, refetchBoards } = useBoards();
 
   return (
     <div className={styles.mainContainer}>
+      <HotBoards />
       <SearchBar refetchBoards={refetchBoards} />
       <div className={styles.boardListContainer}>
         <BoardsList data={boards} refetchBoards={refetchBoards} />
